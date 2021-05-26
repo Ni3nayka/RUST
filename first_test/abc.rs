ae1d6d888c0493a88adcfc0122e7cc2545356195
Ni3nayka <53381511+Ni3nayka@users.noreply.github.com>
2021-05-27 00:52:42 +0300

    add documents

--- first_test/abc.rs
+++ first_test/abc.rs
@@ -1,3 +1,20 @@
+//fn print(x:i32) {
+fn print(x&str) {
+    //println!("{}",x);
+    println!(x)
+}
+
+//fn my_fn(x:i32) {
+
+
+//}
+
 fn main() {
     println!("Hello, world!");
+    let x = 15;
+    let y = "abc";
+    print(str(x));
+    //println!("{}",y);
+    //print(x);
+    //my_fn(x);
 }
--- first_test/main.rs
+++ first_test/main.rs
@@ -1,3 +1,9 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!")
+    //println!("Hello, world!");
+    println!("угадай число");
+    let mut guess = String::new();
+    io::stdin().read_line(&mut guess).expect("строка не считана");
+    println!("вы загадали: {}", guess);
 }
