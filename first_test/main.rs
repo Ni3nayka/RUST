53479d522cf1728fd0181aae7d8073cdbda117f6
Ni3nayka <53381511+Ni3nayka@users.noreply.github.com>
2021-06-02 00:17:41 +0300

    update

--- first_test/main.rs
+++ first_test/main.rs
@@ -2,8 +2,29 @@ use std::io;
 
 fn main() {
     //println!("Hello, world!");
-    println!("угадай число");
+    /*println!("угадай число");
     let mut guess = String::new();
     io::stdin().read_line(&mut guess).expect("строка не считана");
-    println!("вы загадали: {}", guess);
+    println!("вы загадали: {}", guess);*/
+    let n = 86240;
+    println!("answer: {}", prime_factors(n));
+}
+
+//======================================================================================================================================
+//======================================================================================================================================
+
+fn prime_factors(n: i64) -> String {
+    let mut N = n;
+    println!("{}", N);
+    let mut i = 2;
+    while (N>1) {
+        println!("{}", N);
+        let mut ii = 0;
+        while (N%i!=0) {
+            N = N/i;
+        }
+        println!("{}", i, "{}", ii);
+    }
+    let a = "1234";
+    a.to_string()
 }
