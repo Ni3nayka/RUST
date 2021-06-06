7c2dd0597227cbe9c31685a880bbc02c5ba28aeb
Ni3nayka <53381511+Ni3nayka@users.noreply.github.com>
2021-06-06 20:36:42 +0300

    update and add easy number

---
+++ codewars/small_programs/Remove_First_and_Last_Character.rs
@@ -0,0 +1,10 @@
+https://www.codewars.com/kata/56bc28ad5bdaeb48760009b0/train/rust
+
+fn main() {
+    let n: &str = "qwerty";
+    println!("answer: {}", remove_char(n));
+}
+
+fn remove_char(s: &str) -> String {
+    String::from(&s[1..s.len()-1])
+}
