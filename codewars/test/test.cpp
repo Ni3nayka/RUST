cacd24f661d2755f86ae507b9161992d24070797
Ni3nayka <53381511+Ni3nayka@users.noreply.github.com>
2021-06-08 00:03:31 +0300

    add cpp test

---
+++ codewars/test/test.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+//#include <fstream>
+//#include <stdlib.h>
+//#include <vector>
+//#include "windows.h"
+
+//#define FILE 0 // 1 - file; 0 - std
+
+int main() {
+  cout << "Hello" << endl;
+}
